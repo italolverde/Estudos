edd1802c5b4de944b188e248da319ede0b391a72
Ítalo Lima Verde <125528153+oYeiou@users.noreply.github.com>
2023-06-21 12:59:34 -0300

    aa
    aa

---
+++ JS/calculadora/index.js
@@ -0,0 +1,17 @@
+numero = ''
+primeironum = false
+
+function clicknum(num) {
+    numero += String(num)
+    console.log(numero)
+}
+function op(operador) {
+    if (primeironum) {
+        
+    }
+    switch(operador) {
+        case 1 :
+
+        
+    }
+}
