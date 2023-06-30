69bee1795dd16a8bfe8e8f10b63d14b2e8420bfe
Ítalo Lima Verde <125528153+oYeiou@users.noreply.github.com>
2023-06-30 16:09:25 -0300

    Update index.js

--- JS/calculadora/index.js
+++ JS/calculadora/index.js
@@ -1,17 +1,35 @@
-numero = ''
-primeironum = false
+nums = []
+ops = []
+res = 0
+display = document.getElementById('display')
 
 function clicknum(num) {
-    numero += String(num)
-    console.log(numero)
+    nums.push(num)
 }
-function op(operador) {
-    if (primeironum) {
-        //op = 1
-    }
-    switch(operador) {
-        case 1 :
 
-        
-    }
+function clickop(op) {
+    ops.push(op)
 }
+
+function calcular() {
+    res = nums[0]
+    for(i = 1; i<=nums.length; i++) {
+        switch(ops[i-1]) {
+            case 1:
+                res += nums[i]
+                break
+            case 2:
+                res -= nums[i]
+                break
+            case 3:
+                res /= nums[i]
+                break
+            case 4:
+                res *= nums[i]
+                break
+        }
+    }
+    console.log(res)
+    console.log(nums.length)
+    console.log(String(res))
+    display.innerText = 'hiuhg'
