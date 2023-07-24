d50fc0eebeec3367867b394e50d9fea39e13b195
Ítalo Lima Verde <125528153+oYeiou@users.noreply.github.com>
2023-07-24 16:00:13 -0300

    banco js

---
+++ JS/banco/index.js
@@ -0,0 +1,38 @@
+valor = 0
+
+class Cliente {
+    constructor(nome, saldoinicial, id) {
+        this.nome = nome
+        this.saldo = saldoinicial
+        this.id = id
+    }
+
+    csacar(valor, cliente) {
+        this.saldo -= valor
+        console.log(`Foram sacados R$${valor} da conta de ${clientes[cliente].nome}, novo saldo: R$${this.saldo}`)
+        document.getElementById('valor').value = null    
+    }
+    cdepositar(valor) {
+        this.saldo += valor
+        console.log(`Foram depositados R$${valor} da conta de ${clientes[cliente].nome}, novo saldo: R$${this.saldo}`)    
+    }
+}
+clientes = [0]
+console.log(clientes)
+function sacar() {
+    cliente = Number(document.getElementById('cliente').value)
+    valor = Number(document.getElementById('valor').value)
+    clientes[cliente].csacar(valor,cliente)
+}
+function depositar() {
+    cliente = Number(document.getElementById('cliente').value)
+    valor = Number(document.getElementById('valor').value)
+    clientes[cliente].cdepositar(valor,cliente)
+}
+
+function cadastrar() {
+    nome = document.getElementById('nomecadastro').value
+    document.getElementById('nomecadastro').value = null
+    clientes.push(new Cliente(nome, 100, clientes.length))
+    console.log(clientes)
+}
