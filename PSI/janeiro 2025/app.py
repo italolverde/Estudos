299fb1d676b1a084ec0195952e7ae7011fa8d774
Ítalo Lima Verde <125528153+italolverde@users.noreply.github.com>
2025-01-16 16:43:23 +0000

    atividade romerito 16/01

--- PSI/janeiro 2025/app.py
+++ PSI/janeiro 2025/app.py
@@ -1,22 +1,39 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect,url_for
 from database import db
-from models import User
+from models import User,Livros
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///teste2.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///livraria.db'
 
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
-    user = User(nome='mundim')
-    db.session.add(user)
-    db.session.commit()
-    return render_template('listar.html')
+    usuarios = db.session.execute(db.select(User)).scalars()
+    livros = db.session.execute(db.select(Livros)).scalars()
+    return render_template('listar.html', users = usuarios, livros = livros)
+
+@app.route('/cadastro/user', methods=['POST'])
+def cadastro_user():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        user = User(nome=nome)
+        db.session.add(user)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/cadastro/livro', methods=['POST'])
+def cadastro_livro():
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        livro = Livros(titulo=titulo)
+        db.session.add(livro)
+        db.session.commit()
+    return redirect(url_for('index'))
 
 @app.route('/listar')
 def listar():
--- PSI/janeiro 2025/models.py
+++ PSI/janeiro 2025/models.py
@@ -4,3 +4,7 @@ from database import db
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     nome: Mapped[str]
+
+class Livros(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    titulo: Mapped[str]
